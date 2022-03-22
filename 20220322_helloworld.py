4ee5939ddadee2ddadc63247c24dcfe104b35da4
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-03-22 10:14:15 +0900

    변수 선언, 초기화, 여러가지 출력하는 방법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,4 +1,9 @@
-print("Hello, World!")
-print('Hello, 인소리') #큰따옴표와 작은따옴표는 같음
+print("Hello, World!")  #큰따옴표와 작은따옴표는 같음
 print('Red velvet\'s favorite song is Red flavour')  #' <-가 있는 경우 큰따옴표를 써야한다. 아니면 ' 앞에 \를 붙여준다.
 print("Red velvet's favorite song is Red flavour")
+
+name='양다연'
+print('Hello, '+name+'!')   #JAVA 형식
+print('Hello, %s!' % name)  #C 형식
+print('Hello, {}!'.format(name))    #format 함수
+print(f'Hello, {name}!')    #f-string 형식
