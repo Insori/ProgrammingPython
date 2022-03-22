803cf92307d917138fb338253bda577c0d27d233
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-03-22 10:03:06 +0900

    helloworld, print()

---
+++ 20220322_helloworld.py
@@ -0,0 +1,4 @@
+print("Hello, World!")
+print('Hello, 인소리') #큰따옴표와 작은따옴표는 같음
+print('Red velvet\'s favorite song is Red flavour')  #' <-가 있는 경우 큰따옴표를 써야한다. 아니면 ' 앞에 \를 붙여준다.
+print("Red velvet's favorite song is Red flavour")
