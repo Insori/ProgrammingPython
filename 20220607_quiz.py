fda00d1dca2c98b455db8c41aa8af7c30a4e7ab6
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-06-07 09:55:46 +0900

    Quiz. 학번 -> 학년, 반, 학과, 번호

--- 20220607_quiz.py
+++ 20220607_quiz.py
@@ -13,3 +13,18 @@ print(phone_number)
 
 print('-'*20)
 
+# 2. 학번 -> 학년, 반, 학과, 번호 출력하기
+student_number='2208'
+# if student_number[1]=='1' or student_number[1]=='2':
+#     h='뉴미디어소프트웨어과'
+# elif student_number[1]=='3' or student_number[1]=='4':
+#     h='뉴미디어웹솔루션과'
+# elif student_number[1]=='5' or student_number[1]=='6':
+#     h='뉴미디어디자인과'
+majors = ['','뉴미디어소프트웨어과', '뉴미디어소프트웨어과', '뉴미디어웹솔루션과', '뉴미디어웹솔루션과', '뉴미디어디자인과', '뉴미디어디자인과']
+index = int(student_number[1])
+h = majors[index]
+print(f'{student_number[0]}학년 {student_number[1]}반 {h} {int(student_number[2:])}번')
+
+print('-'*20)
+
