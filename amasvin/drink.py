263c65be59f720d86acc01edd6f14747c97724e6
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-10-18 10:20:24 +0900

    start Drink

---
+++ amasvin/drink.py
@@ -0,0 +1,16 @@
+# Drink
+    # name
+    # price
+    # cup size
+    # sugar
+    # ice
+
+# Drink <- Coffee
+# Drink <- Bubbletea
+    # pearl
+
+# 옵션
+# 컵 사이즈: 기본, 점보
+# 펄: 타피오카, 알로에, 콘약, 코코넛
+# 당도: 30%, 50%, 70%, 100%
+# 얼음양: 없음, 적게, 기본, 많게
