184a4ffc486369c022c9e8e6927fca85ed2fb09a
Insori <s2110@e-mirim.hs.kr>
2022-11-22 09:09:23 +0900

    order 주문 내역 출력, 가격 총 합계 출력, amasvin에서 실행

--- amasvin/order.py
+++ amasvin/order.py
@@ -11,8 +11,11 @@ class Order:
 
     def __str__(self):
         # self.order_menu에서 drink 하나씩 꺼내서 출력하자
-        return '\n'.join((str(drink) for drink in self.order_menu))
         # 총 주문 금액 출력
+        total_price = 0
+        for drink in self.order_menu:
+            total_price += drink.price
+        return '\n'.join((str(drink) for drink in self.order_menu))+f'\n총 주문 금액은 {total_price}원입니다.'
 
     def init_menu(self):    # 메뉴판 초기화
         new_menu = Bubbletea("하동녹차오레오", 4500)
@@ -35,11 +38,14 @@ class Order:
             new_drink = copy(self.menu[int(choice) - 1])
                 # 메뉴 그대로 가져오면, 옵션에 따라 메뉴가 바뀌어 있음
             # 메뉴는 기본으로 놔두고, 복사해와서 그것의 옵션을 바꾸자
-            new_drink.order()
+            new_drink.order()   # 음료 주문 받자(옵션)
             # print(new_drink)
             # 주문한 음료수 리스트에 새로운 음료수 추가하기
             self.order_menu.append(new_drink)
         # 주문한 음료수 리스트 출력
+        print("-" * 35)
+        print("주문하신 음료수는 다음과 같습니다.")
+        print(self)
 
     def show_menu(self):
         for index, drink in enumerate(self.menu):
@@ -48,4 +54,3 @@ class Order:
 if __name__ == '__main__':
     order = Order()
     order.order()
-    print(order)
